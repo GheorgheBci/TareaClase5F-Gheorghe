a02f6d47b967f3078003cc5f93d0c86351991ae1
Gheorghe <georgebucu96@gmail.com>
2021-03-03 20:10:29 +0100

    Crea 15 objetos CuadradoMagico, muestra su contenido y si son cuadrados mágicos

--- src/main/java/daw/gheorghe/Prueba.java
+++ src/main/java/daw/gheorghe/Prueba.java
@@ -5,21 +5,99 @@ package daw.gheorghe;
  * @author Jorge
  */
 public class Prueba {
-    
+
     public static void main(String[] args) {
-        
+
         CuadradoMagico cm1 = new CuadradoMagico();
-        
+
         cm1.imprimirMatriz();
-        
+
         System.out.println("La suma total de la fila es: " + cm1.sumarFila(0));
-        
+
         System.out.println("La suma total de la columna es: " + cm1.sumarColumna(1));
-        
+
         System.out.println("La suma total de la diagonal principal es: " + cm1.sumarDiagonalPrincipal());
-        
+
         System.out.println("La suma total de la diagonal secundaria es: " + cm1.sumarDiagonalSecundaria());
-        
+
         System.out.println("¿Es un cuadrado mágico? " + cm1.esCuadradoMagico());
+        
+        System.out.println("");
+
+        CuadradoMagico cm2 = new CuadradoMagico();
+        cm2.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm2.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm3 = new CuadradoMagico();
+        cm3.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm3.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm4 = new CuadradoMagico();
+        cm4.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm4.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm5 = new CuadradoMagico();
+        cm5.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm5.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm6 = new CuadradoMagico();
+        cm6.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm6.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm7 = new CuadradoMagico();
+        cm7.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm7.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm8 = new CuadradoMagico();
+        cm8.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm8.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm9 = new CuadradoMagico();
+        cm9.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm9.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm10 = new CuadradoMagico();
+        cm10.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm10.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm11 = new CuadradoMagico();
+        cm11.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm11.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm12 = new CuadradoMagico();
+        cm12.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm12.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm13 = new CuadradoMagico();
+        cm13.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm13.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm14 = new CuadradoMagico();
+        cm14.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm14.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm15 = new CuadradoMagico();
+        cm15.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm15.esCuadradoMagico());
+        System.out.println("");
+
+        CuadradoMagico cm16 = new CuadradoMagico();
+        cm16.imprimirMatriz();
+        System.out.println("¿Es un cuadrado mágico? " + cm16.esCuadradoMagico());
+        System.out.println("");
+
     }
 }
